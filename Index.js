96ec0bb5b48068c966f64e38f90abe3da6b573a4
hieudn1995 <53845464+hieudn1995@users.noreply.github.com>
2019-08-08 02:10:27 +0700

    Update and rename tut to Index.js

---
+++ Index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+cnst frefix ="&"
+
+client.on("ready",fuction(){
+bit.user.setActiviti("Help")
+Console.log("hello");
+)};
+
+client.on("message",(message) ==>{
+If(message.content.startswith(frefix + "ping")){
+message.channel.send("hello");
+)}};
+client.login(process.env.token);
